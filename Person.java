90b0a00ea2895d71447ac3c936a2eb0e23dc9b4d
pkkoshta <prashant30koshta@gmail.com>
2022-11-25 13:52:00 +0530

    added person.java file added 4 lines

--- Person.java
+++ Person.java
@@ -2,6 +2,7 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hi pushed to feature breanch");
-       
+        System.out.println("Hello world");
+        System.out.println("Hi pushed to feature breanch");
     }
 }
