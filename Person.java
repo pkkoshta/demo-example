09a1fc3e30a7cde755f111d70b01a3c6ee83f404
Pkkoshta <62608305+pkkoshta@users.noreply.github.com>
2022-11-25 13:41:05 +0530

    Update Person.java
    deleted wrong sout

--- Person.java
+++ Person.java
@@ -2,7 +2,6 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hi pushed to feature breanch");
-        System.out.println("Hello world");
-        System.out.println("Hi pushed to feature breanch");
+       
     }
 }
